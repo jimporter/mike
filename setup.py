5e8a410be95800471e206ea1a9863e82034c3ea9
Jim Porter <itsjimporter@gmail.com>
2019-05-25 21:39:31 -0700

    Fix Travis builds

--- setup.py
+++ setup.py
@@ -92,9 +92,9 @@ setup(
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
 
     packages=find_packages(exclude=['test', 'test.*']),
