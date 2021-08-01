205e05238ff6a40d8e8323c4ae4d42f4f85c0fc9
Jim Porter <itsjimporter@gmail.com>
2021-07-31 22:05:16 -0700

    Update "development status" classifier

--- setup.py
+++ setup.py
@@ -77,7 +77,7 @@ setup(
     license='BSD',
 
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
 
         'Intended Audience :: Developers',
 
