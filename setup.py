49cf9c8925d86847b6dbfbf82ce6fa4aa428e4e0
Jim Porter <itsjimporter@gmail.com>
2022-12-06 20:33:20 -0800

    Test Python 3.11 in CI

--- setup.py
+++ setup.py
@@ -80,6 +80,7 @@ setup(
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
     ],
 
     packages=find_packages(exclude=['test', 'test.*']),
