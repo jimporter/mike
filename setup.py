6c21c4c2c26ffb4052d69780ce847fd467f6c582
Francisco Serrano <francisco.serrano@upcloud.com>
2026-02-12 17:05:17 +0200

    Fix CVE-2024-5569: Pin zipp >= 3.19.1 to address infinite loop vulnerability

--- setup.py
+++ setup.py
@@ -88,7 +88,8 @@ setup(
 
     install_requires=(['importlib_metadata', 'importlib_resources',
                        'jinja2 >= 2.7', 'mkdocs >= 1.0', 'pyparsing >= 3.0',
-                       'pyyaml >= 5.1', 'pyyaml_env_tag', 'verspec']),
+                       'pyyaml >= 5.1', 'pyyaml_env_tag', 'verspec',
+                       'zipp >= 3.19.1']),
     extras_require={
         'dev': ['coverage', 'flake8 >= 3.0', 'flake8-quotes', 'shtab'],
         'test': ['coverage', 'flake8 >= 3.0', 'flake8-quotes', 'shtab'],
